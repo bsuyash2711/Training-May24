1ad550c6a2a8cf55d5e73f2eebad52f1770a313e
bsuyash2711 <bagadesuyash2000@gmail.com>
2024-05-23 09:02:18 +0000

    sum program

---
+++ sum.c
@@ -0,0 +1,14 @@
+/* Sum of Numbers 
+ DOC 23 rd may*/
+#include <stdio.h>
+int main(){
+	int a;
+	int b;
+	printf("enter first number:");
+	scanf("%d",&a);
+	printf("enter second number:");
+	scanf("%d",&b);
+	printf("sum of two numbers is : %d",a+b);
+	return 0;
+}
+
