fefebb8fc6367ff95116e89decbf13b231fb7ada
bsuyash2711 <bagadesuyash2000@gmail.com>
2024-05-23 08:53:50 +0000

    sort program

---
+++ sort.c
@@ -0,0 +1,14 @@
+/*
+ Program to sum of numbers
+DOC 23 MAY */
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	printf("enter first number :);
+	scanf("%d",&a);
+	printf("enter second number:");
+	scanf("%d",&b);
+	printf("sum of numbers is %d",a+b);
+	return 0;
+}
